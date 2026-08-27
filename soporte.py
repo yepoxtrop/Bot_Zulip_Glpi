dafe13dd8d34b5a39ae519dee6de4b690aa24114
neo <lsarmiento@aciel.co>
2026-08-27 12:34:43 -0500

    add connection to api zulip to upload scripts

--- soporte.py
+++ soporte.py
@@ -346,19 +346,12 @@ class SoporteHandler():
                     content = "".join(map(str, Messages.MESSAGE_RUSDESK_URL_FILE.value));
                     content = content.replace("[URL_ARCHIVO]", list_files[0]);
                     
-                    content_image = "".join(map(str, Messages.MESSAGE_IMG.value));
-                    content_image = content_image.replace("[URL_IMAGEN]", list_files[1]);
-                    message_to_send = {
-                        'type': 'stream',
-                        'to': message['display_recipient'],
-                        'subject': message['subject'],
-                        'content': content_image,
-                        'content_type': 'text/html'
-                    }
+                    content_image = f"![windows.png]({list_files[1]})"
+                    # bot_handler.send_reply(message, content_image)
                     
                      
                     bot_handler.send_reply(message, content);
-                    bot_handler.send_reply(message, message_to_send);
+                    bot_handler.send_reply(message, content_image);
                     
                     bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
                             "name":message["sender_full_name"], 
