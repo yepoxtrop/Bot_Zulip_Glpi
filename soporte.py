956fecf43e2f4e4cd55107832bd7772c75b9499a
neo <lsarmiento@aciel.co>
2026-08-28 11:11:12 -0500

    update command hel

--- soporte.py
+++ soporte.py
@@ -13,7 +13,7 @@ from src.models.urgencia import Urgencia;
 from src.models.messages import Messages;
 
 # Constantes creadas
-from src.settings.settings import ZULIP_URL;
+from src.settings.settings import ZULIP_URL, DOMINIO_CORPORATIVO;
 
 class SoporteHandler():
     def usage(self) -> str:
@@ -28,11 +28,11 @@ class SoporteHandler():
         print(message)
         
         # Si no existe la llave, la crea
-        if not bot_handler.storage.contains(f"{message['sender_full_name']}@aciel.co"):
+        if not bot_handler.storage.contains(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}"):
 
-            # Almacenamiento de clave de usuario nombre@aciel.co
+            # Almacenamiento de clave de usuario nombre@DOMINIO_CORPORATIVO
             bot_handler.storage.put(
-                f"{message['sender_full_name']}@aciel.co", {
+                f"{message['sender_full_name']}@{{DOMINIO_CORPORATIVO}}", {
                     "name":message["sender_full_name"], 
                     "email":message["sender_email"],
                     "process": None, 
@@ -47,13 +47,13 @@ class SoporteHandler():
         # - Validar el estado del caso y actualizaciones("posteriormente")
         #   - Obtener informacion del caso
         #   - Obtener informacion de soporte - contacto
-        if bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["process"] == "/ayuda" or message["full_content"].lower() == "/ayuda":
+        if bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["process"] == "/ayuda" or message["full_content"].lower() == "/ayuda":
             
             # Si el comando no esta inicializado
-            if bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["step"] == None:
+            if bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["step"] == None:
                 content = "".join(map(str, Messages.MESSAGE_AYUDA_WELCOME.value));
                 
-                bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                         "name":message["sender_full_name"], 
                         "email":message["sender_email"],
                         "process":"/ayuda", 
@@ -65,13 +65,13 @@ class SoporteHandler():
             
             # Si esta activo el proceso de 'caso'
             # Se pregunta al usuario si tiene un caso activo
-            elif bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["step"] == "caso":
+            elif bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["step"] == "caso":
 
                 # Si la respuesta es '/si', se le pregunta a la persona si tiene el id
                 # Se cambia el step a 'id_ticket'
                 if message["full_content"].lower() == "/si":
                     bot_handler.send_reply(message, "".join(map(str, Messages.MESSAGE_AYUDA_TICKET_YES.value)));
-                    bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                    bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                             "name":message["sender_full_name"], 
                             "email":message["sender_email"],
                             "process":"/ayuda", 
@@ -84,7 +84,7 @@ class SoporteHandler():
                 # Se cambia el step a 'crear_ticker'
                 elif message["full_content"].lower() == "/no":
                     bot_handler.send_reply(message, "".join(map(str, Messages.MESSAGE_CASO_CHANELS.value)));
-                    bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                    bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                             "name":message["sender_full_name"], 
                             "email":message["sender_email"],
                             "process":None, 
@@ -97,73 +97,71 @@ class SoporteHandler():
 
             # Si esta activo el proceso de 'caso'
             # Se pregunta al usuario si tiene el id del caso
-            elif bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["step"] == "id_ticket":
+            elif bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["step"] == "id_ticket":
                 
                 list_message = message["full_content"].split("/");
                 print(list_message)
                 if len(list_message) == 2 and list_message[0].lower() == "":
                     
                     bot_handler.send_reply(message, "Se esta consultando el ticket");
-                    info_ticket = find_ticket(int(list_message[1]));
-                    print(info_ticket)
+                    try:
+                        code_ticket = int(list_message[1]);
                     
-                    # Si no encuentra ningun ticket con ese id
-                    if (info_ticket == []):
-                        bot_handler.send_reply(message, "Ticket no encontrado");
-                    else:
-                        
-                        menssage_final = "".join(map(str, Messages.MESSAGE_INFO_TICKET.value))
-                        menssage_final = menssage_final.replace("[ID_TICKET]", str(info_ticket[0][0]));
-                        menssage_final = menssage_final.replace("[TITULO_TICKET]", info_ticket[0][1]);
-                        menssage_final = menssage_final.replace("[PRIORIDAD]", str(info_ticket[0][3]));
-                        menssage_final = menssage_final.replace("[CATEGORIA]", str(info_ticket[0][4]));
-                        menssage_final = menssage_final.replace("[ENTIDAD]", info_ticket[0][5]);
-                        menssage_final = menssage_final.replace("[SOLICITANTES]", info_ticket[0][6]);
-                        menssage_final = menssage_final.replace("[CATEGORIA_TICKET]", info_ticket[0][7]);
-                        menssage_final = menssage_final.replace("[TECNICOS]", info_ticket[0][9]);
-                        
-                        bot_handler.send_reply(message, "Ticket Consultado.");
-                        bot_handler.send_reply(message,  menssage_final);
-                        
-                        bot_handler.send_message({
-                            "type": "stream",
-                            "to": "Soporte",
-                            "subject": "Consulta de Soporte Técnico",
-                            "content": f"`@**all** ` Hola compañeros, {message['sender_full_name']} acaba de consultar sobre su ticket, por favor alguno comuniquese con el colaborador para brindarle soporte.",
-                        })
-                        
-                        content = "".join(map(str, Messages.MESSAGE_RUSDESK_OS.value));
-                        bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
-                                "name":message["sender_full_name"], 
-                                "email":message["sender_email"],
-                                "process":"/rustdesk", 
-                                "step":"os", 
-                                "is_completed": False
-                            }
-                        );
-                        bot_handler.send_reply(message, content);
+                        info_ticket = find_ticket(code_ticket);
+                    
+                    
+                        # Si no encuentra ningun ticket con ese id
+                        if (info_ticket == []):
+                            bot_handler.send_reply(message, "Ticket no encontrado");
+                        else:
 
-                    # bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
-                    #         "name":None, 
-                    #         "email":None,
-                    #         "process":None, 
-                    #         "step":None, 
-                    #         "is_completed": None
-                    #     }
-                    # );
+                            menssage_final = "".join(map(str, Messages.MESSAGE_INFO_TICKET.value))
+                            menssage_final = menssage_final.replace("[ID_TICKET]", str(info_ticket[0][0]));
+                            menssage_final = menssage_final.replace("[TITULO_TICKET]", info_ticket[0][1]);
+                            menssage_final = menssage_final.replace("[PRIORIDAD]", str(info_ticket[0][3]));
+                            menssage_final = menssage_final.replace("[CATEGORIA]", str(info_ticket[0][4]));
+                            menssage_final = menssage_final.replace("[ENTIDAD]", info_ticket[0][5]);
+                            menssage_final = menssage_final.replace("[SOLICITANTES]", info_ticket[0][6]);
+                            menssage_final = menssage_final.replace("[CATEGORIA_TICKET]", info_ticket[0][7]);
+                            menssage_final = menssage_final.replace("[TECNICOS]", info_ticket[0][9]);
+
+                            bot_handler.send_reply(message, "Ticket Consultado.");
+                            bot_handler.send_reply(message,  menssage_final);
+
+                            bot_handler.send_message({
+                                "type": "stream",
+                                "to": "Soporte",
+                                "subject": "Consulta de Soporte Técnico",
+                                "content": f"`@**all** ` Hola compañeros, {message['sender_full_name']} acaba de consultar sobre su ticket, por favor alguno comuniquese con el colaborador para brindarle soporte.",
+                            })
+
+                            content = "".join(map(str, Messages.MESSAGE_RUSDESK_OS.value));
+                            bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
+                                    "name":message["sender_full_name"], 
+                                    "email":message["sender_email"],
+                                    "process":"/rustdesk", 
+                                    "step":"os", 
+                                    "is_completed": False
+                                }
+                            );
+                            bot_handler.send_reply(message, content);
+                            
+                    except Exception as e:
+                        bot_handler.send_reply(message, "No se pudo realizar correctamente la consulta");
+
                 else:
-                    bot_handler.send_reply(message, "Comando no válido, recuerda que las opciones validas son: ");           
+                    bot_handler.send_reply(message, "Comando no válido, recuerda que las opción valida es:\n `/<numero_ticket>` (ejemplo: `/4168`)");           
             
         # Comando /caso -> Comando principal para:
         # - Crear casos - Para versiones posteriores
         # - Ofrecer canales de comunicacion
-        elif bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["process"] == "/caso" or message["full_content"].lower() == "/caso":
+        elif bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["process"] == "/caso" or message["full_content"].lower() == "/caso":
             print("Entramos al condicional de /caso")
             # Si el comando no esta inicializado
-            if bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["step"] == None:
+            if bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["step"] == None:
                 # content = "".join(map(str, Messages.MESSAGE_CASO_WELCOME.value));
                 status_dict = bot_handler.storage.get("status");
-                bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                         "name":message["sender_full_name"], 
                         "email":message["sender_email"],
                         "process":"/caso", 
@@ -173,7 +171,7 @@ class SoporteHandler():
                 );
             content = "".join(map(str, Messages.MESSAGE_CASO_CHANELS.value));
             bot_handler.send_reply(message, content);
-            bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+            bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                     "name":message["sender_full_name"], 
                     "email":message["sender_email"],
                     "process":None, 
@@ -185,12 +183,12 @@ class SoporteHandler():
         
         # Comando /rustdesk -> Comando principal para:
         # - Verificar el codigo de rustdesk segun el os
-        elif bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["process"] == "/rustdesk" or message["full_content"].lower() == "/rustdesk":
+        elif bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["process"] == "/rustdesk" or message["full_content"].lower() == "/rustdesk":
             
             # Si el comando no esta inicializado
-            if bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["step"] == None:
+            if bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["step"] == None:
                 content = "".join(map(str, Messages.MESSAGE_RUSDESK_OS.value));
-                bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                         "name":message["sender_full_name"], 
                         "email":message["sender_email"],
                         "process":"/rustdesk", 
@@ -202,7 +200,7 @@ class SoporteHandler():
                    
             # Si esta activo el proceso de 'os'
             # Se pregunta al usuario si tiene un caso activo
-            elif bot_handler.storage.get(f"{message['sender_full_name']}@aciel.co")["step"] == "os":
+            elif bot_handler.storage.get(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}")["step"] == "os":
 
                 # Si la respuesta es '/windows'
                 # Se hace una petición a al api de zulip, se carga el archivo y se envia atachado
@@ -230,7 +228,7 @@ class SoporteHandler():
                     bot_handler.send_reply(message, content);
                     bot_handler.send_reply(message, image_url);
 
-                    bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                    bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                             "name":message["sender_full_name"], 
                             "email":message["sender_email"],
                             "process":None, 
@@ -264,7 +262,7 @@ class SoporteHandler():
                     bot_handler.send_reply(message, content);
                     bot_handler.send_reply(message, image_url);
                     
-                    bot_handler.storage.put(f"{message['sender_full_name']}@aciel.co", {
+                    bot_handler.storage.put(f"{message['sender_full_name']}@{DOMINIO_CORPORATIVO}", {
                             "name":message["sender_full_name"], 
                             "email":message["sender_email"],
                             "process":None, 
